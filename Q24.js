972ce680fcbd241f53620d1094b2399897339a7f
Muhammad Naeem Warsi <161728783+MuhammadNaeemWarsi@users.noreply.github.com>
2024-04-06 18:37:22 +0500

    Q24 complete

---
+++ Q24.js
@@ -0,0 +1,34 @@
+// More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+//• Tests for equality and inequality with strings
+//• Tests using the lower case function
+//• Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+//• Tests using "and" and "or" operators
+//• Test whether an item is in a array
+// • Test whether an item is not in a array
+// equality and inequality
+var string1 = "Naeem";
+var string2 = "Raheel";
+//console.log(string1 === string2);
+//console.log(string1 !== string2);
+//Tests using the lower case function
+var mixedCaseString = "Naeem Warsi";
+// console.log(mixedCaseString.toLowerCase() ==="naeem warsi")
+// Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+var num1 = 10;
+var num2 = 6;
+//console.log(num1 === num2)
+//console.log(num1 !== num2)
+//console.log(num1 > num2)
+//console.log(num1 < num2)
+//console.log(num1 >= num2)
+//console.log(num1 <= num2)
+// Tests using "and" and "or" operators
+var condition1 = true;
+var condition2 = false;
+//console.log(condition1 && condition2)
+//console.log(condition1 || condition2)
+// Test whether an item is in a array
+var Fruits = ["Mango", "Orange", "Banana", "Melon"];
+console.log(Fruits.includes("Mango"));
+console.log(Fruits.includes("mango"));
+// Test whether an item is not in a array
